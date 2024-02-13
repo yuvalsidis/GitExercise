ed66845ad291164cf20da3e45c21a2e5ff7fefba
yuvalsidis <yuvalsidis@gmail.com>
2024-02-13 22:20:00 +0200

    Add limit of 400px for ball onBallClick()

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ var heightBall1 = 100
 
 function onBallClick(elm){
     const elmP = document.querySelector('.ball p')
-    if(widthBall1 < 300){
+    if(widthBall1 === 400){
+        widthBall1 = 50
+        heightBall1 = 50
+    }
+    if(widthBall1 < 400){
     widthBall1 += 50
     heightBall1 += 50
     elm.style.width = widthBall1 + 'px'
