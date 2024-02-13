c17cf82bea45c4fc1a84c84af2010d826ca70c08
yuvalsidis <yuvalsidis@gmail.com>
2024-02-13 22:39:12 +0200

    Modify random increment onBallClick()

--- main.js
+++ main.js
@@ -3,16 +3,25 @@ var widthBall1 = 100
 var heightBall1 = 100
 
 function onBallClick(elm){
+    const randomNumber = getRandomInt(20, 61)
+
     const elmP = document.querySelector('.ball p')
-    if(widthBall1 === 400){
-        widthBall1 = 50
-        heightBall1 = 50
+    if(widthBall1 >= 400){
+        widthBall1 = randomNumber
+        heightBall1 = randomNumber
     }
     if(widthBall1 < 400){
-    widthBall1 += 50
-    heightBall1 += 50
+    widthBall1 += randomNumber
+    heightBall1 += randomNumber
     elm.style.width = widthBall1 + 'px'
     elm.style.height = heightBall1 +'px'
     elmP.innerText = `width : ${elm.style.width} height: ${elm.style.height}`
     }
 }
+
+
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+  }
