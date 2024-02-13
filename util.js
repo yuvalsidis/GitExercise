48497de2b9f6f67ce18492bdb5080ab87ceb94ca
yuvalsidis <yuvalsidis@gmail.com>
2024-02-13 22:35:25 +0200

    Add utilities 'util.js' file

---
+++ util.js
@@ -0,0 +1,7 @@
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+  }
+
+  
