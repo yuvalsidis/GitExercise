451356eb54de10c787958945d2c68d847d038ae1
yuvalsidis <yuvalsidis@gmail.com>
2024-02-13 23:59:03 +0200

    Modify be More generic function  onBallClick()

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 'use strict'
-var widthBall1 = 100
-var heightBall1 = 100
+
 
 function onBallClick(elm){
+    var elmWidth = +elm.style.width.replace('px','') 
+    var elmHeight = +elm.style.height.replace('px','') 
+    var elmId = elm.id
+    console.log(elmWidth)
+    console.log(elmHeight)
+
     const randomNumber = getRandomInt(20, 61)
     const randomColor = getRandomColor()
 
     elm.style.backgroundColor = randomColor
-    const elmP = document.querySelector('.ball p')
-    if(widthBall1 >= 400){
-        widthBall1 = randomNumber
-        heightBall1 = randomNumber
+    const elmP = document.querySelector(`.${elmId} p`)
+    if(elmWidth >= 400){
+        elmWidth = randomNumber + 100
+        elmHeight = randomNumber + 100
     }
-    if(widthBall1 < 400){
-    widthBall1 += randomNumber
-    heightBall1 += randomNumber
-    elm.style.width = widthBall1 + 'px'
-    elm.style.height = heightBall1 +'px'
-    elmP.innerText = `width : ${elm.style.width} height: ${elm.style.height}`
+    else if(elmWidth < 400){
+    elmWidth += randomNumber 
+    elmHeight += randomNumber
     }
+    elm.style.width = elmWidth + 'px'
+    elm.style.height = elmHeight +'px'
+    elmP.innerText = `Width : ${elm.style.width} height: ${elm.style.height}`
 }
